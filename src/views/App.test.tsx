47b4599bc6e47c5799127301da41f0b0c1646ce0
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-01 20:14:13 +0100

    - Simple test for path routing added

--- src/views/App.test.tsx
+++ src/views/App.test.tsx
@@ -3,9 +3,12 @@ import App from './App';
 import {MemoryRouter} from 'react-router-dom'
 import {render} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import {DayViewProps} from "./DayView";
+
+jest.mock("./DayView", () => (props: DayViewProps): React.ReactNode => <div>{props.day}</div>);
 
 describe("App", () => {
-    it("should render a day view ", async () => {
+    it("should render a day view with a property set to the according path", async () => {
         const wrapper = render(
             <MemoryRouter initialEntries={['/days/morgen']}>
                 <App/>
