7e00d544a7101b44f8c775c4fd50d00177f34e61
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-06 18:26:31 +0100

    - Reading of allready stored timelogs implemented

--- src/local/TimeLogRepository.ts
+++ src/local/TimeLogRepository.ts
@@ -1,7 +1,22 @@
 import {TimeLog} from "../core/TimeLogService";
+import Lockr from "lockr";
+import moment from "moment";
+
+interface TimeLogDbo {
+    [day: string]: TimeLog[]
+}
 
 export default class TimeLogRepository {
     public static getTimeLogsForDay(date: Date): TimeLog[] {
-        return [{day: new Date(), durationInMinutes: 42, description: "foo"}];
+
+        const timeLog: TimeLogDbo = Lockr.get("TimeLog", {});
+
+        const key = this.timelogKeyFor(date);
+        const timelogs = timeLog[key];
+        return timelogs ? timelogs : [];
+    }
+
+    private static timelogKeyFor(date: Date) {
+        return moment(date).format("YYYY-MM-DD");
     }
 }
