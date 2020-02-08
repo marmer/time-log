36c7ee766f498a9d2eb675a5f1eec429778211fc
marmer <mariano.mertinat@gmail.com>
2020-02-08 23:43:56 +0100

    docs directory for github pages created

---
+++ docs/precache-manifest.b122b282bd5854fef39387086f1ffc9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5beab7a36a60aef0f9897ffea891134b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9127834ba658ad9d81",
+    "url": "/static/css/2.08c74b9a.chunk.css"
+  },
+  {
+    "revision": "327777bcf55a315f0adc",
+    "url": "/static/css/main.06bce988.chunk.css"
+  },
+  {
+    "revision": "eb9127834ba658ad9d81",
+    "url": "/static/js/2.b4c8c225.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.b4c8c225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327777bcf55a315f0adc",
+    "url": "/static/js/main.83414d5f.chunk.js"
+  },
+  {
+    "revision": "173131f8be94fe8b607b",
+    "url": "/static/js/runtime-main.ce57f013.js"
+  }
+]);
