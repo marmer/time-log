bf32f348c070038fd33356462bb313a57ffca7f6
marmer <mariano.mertinat@gmail.com>
2020-02-09 01:07:33 +0100

    basepath changed

---
+++ docs/precache-manifest.6352c63e0747dfbeb8296d61630e97a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d43c8e58a112e01f561e294fa43262b",
+    "url": "/time-log/index.html"
+  },
+  {
+    "revision": "eb9127834ba658ad9d81",
+    "url": "/time-log/static/css/2.08c74b9a.chunk.css"
+  },
+  {
+    "revision": "49ea4eb2eda6b89ed5a4",
+    "url": "/time-log/static/css/main.06bce988.chunk.css"
+  },
+  {
+    "revision": "eb9127834ba658ad9d81",
+    "url": "/time-log/static/js/2.b4c8c225.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/time-log/static/js/2.b4c8c225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ea4eb2eda6b89ed5a4",
+    "url": "/time-log/static/js/main.451c2866.chunk.js"
+  },
+  {
+    "revision": "8bba61cab2cc2ca10a76",
+    "url": "/time-log/static/js/runtime-main.298d46d2.js"
+  }
+]);
