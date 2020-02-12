bb80473d5a20fda73c3a458bab501f3db723593a
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-12 17:54:59 +0100

    - a little step to OAuth2

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,10 +5,32 @@ import NotFoundView from "./NotFoundView";
 import moment from "moment";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const LoginSpike = () => {
+
+
+    https://accounts.google.com/o/oauth2/v2/auth
+
+        if (!process.env.REACT_APP_OAUTH_CLIENT_ID)
+            return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_CLIENT_ID</div>
+    if (!process.env.REACT_APP_OAUTH_CLIENT_SECRET)
+        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_CLIENT_SECRET</div>
+    if (!process.env.REACT_APP_OAUTH_REDIRECT_URL)
+        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_REDIRECT_URL</div>
+
+
+    return (<div><p>This yould be a login</p>
+        <p>{process.env.REACT_APP_OAUTH_CLIENT_ID}</p>
+        <p>{process.env.REACT_APP_OAUTH_CLIENT_SECRET}</p>
+        <p>{process.env.REACT_APP_OAUTH_REDIRECT_URL}</p>
+    </div>)
+}
+
 export default class App extends React.Component {
     render() {
         return (
             <main>
+                <LoginSpike/>
+
                 <Route render={props => {
                     props.history.listen(() => window.location.reload());
                     return <Switch>
