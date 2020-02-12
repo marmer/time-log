e1c631d082298e5f082e448b82fa417036d06d93
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-12 15:50:59 +0100

    - Step back to a browser router

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import "./index.css"
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter><App/></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
