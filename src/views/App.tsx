82ef9d2514feb91d43a124053b5f23839115fa70
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-13 13:50:45 +0100

    - Refactoring

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -4,45 +4,7 @@ import DayView from "./DayView";
 import NotFoundView from "./NotFoundView";
 import moment from "moment";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-const LoginSpike = () => {
-    var OAuth = require('@zalando/oauth2-client-js');
-    var google = new OAuth.Provider({
-        id: 'google',   // required
-        authorization_url: 'https://accounts.google.com/o/oauth2/v2/auth' // required,
-    });
-
-    // https://www.npmjs.com/package/@zalando/oauth2-client-js
-    if (!process.env.REACT_APP_OAUTH_CLIENT_ID)
-        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_CLIENT_ID</div>
-    if (!process.env.REACT_APP_OAUTH_CLIENT_SECRET)
-        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_CLIENT_SECRET</div>
-    if (!process.env.REACT_APP_OAUTH_REDIRECT_URL)
-        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_REDIRECT_URL</div>
-
-
-    return (<div><p>This yould be a login</p>
-        <button onClick={() => {
-            // Create a new request
-            var request = new OAuth.Request({
-                client_id: process.env.REACT_APP_OAUTH_CLIENT_ID,  // required
-                redirect_uri: process.env.REACT_APP_OAUTH_REDIRECT_URL,
-                scope: "email https://www.googleapis.com/auth/drive.file",
-            });
-
-// Give it to the provider
-            var uri = google.requestToken(request);
-
-// Later we need to check if the response was expected
-// so save the request
-            google.remember(request);
-
-// Do the redirect
-            window.location.href = uri
-        }}>login with google
-        </button>
-    </div>)
-}
+import {UserView} from "./UserView";
 
 interface GoogleUserInfo {
     id: string;
@@ -73,7 +35,7 @@ export default function App() {
 
     return (
         <main>
-            <LoginSpike/>
+            <UserView/>
 
             <Route render={props => {
                 props.history.listen(() => window.location.reload());
---
+++ src/views/UserView.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const OAuth = require('@zalando/oauth2-client-js');
+
+export const UserView = () => {
+    const google = new OAuth.Provider({
+        id: 'google',   // required
+        authorization_url: 'https://accounts.google.com/o/oauth2/v2/auth' // required,
+    });
+
+    // https://www.npmjs.com/package/@zalando/oauth2-client-js
+    if (!process.env.REACT_APP_OAUTH_CLIENT_ID)
+        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_CLIENT_ID</div>
+    if (!process.env.REACT_APP_OAUTH_CLIENT_SECRET)
+        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_CLIENT_SECRET</div>
+    if (!process.env.REACT_APP_OAUTH_REDIRECT_URL)
+        return <div>Server Missconfiguration ... Environment Variable not set: REACT_APP_OAUTH_REDIRECT_URL</div>
+
+
+    return (<div><p>This yould be a login</p>
+        <button onClick={() => {
+            const request = new OAuth.Request({
+                client_id: process.env.REACT_APP_OAUTH_CLIENT_ID,  // required
+                redirect_uri: process.env.REACT_APP_OAUTH_REDIRECT_URL,
+                scope: "email https://www.googleapis.com/auth/drive.file",
+            });
+
+            const uri = google.requestToken(request);
+            google.remember(request);
+            window.location.href = uri
+        }}>login with google
+        </button>
+    </div>)
+};
