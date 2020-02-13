8be43fd573d15e93364543e9aa524ca805ab2e15
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-13 14:03:23 +0100

    - Refactoring

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,63 +5,9 @@ import NotFoundView from "./NotFoundView";
 import moment from "moment";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {UserView} from "./UserView";
-
-interface GoogleUserInfo {
-    id: string;
-    email: string;
-    verified_email: boolean;
-    picture: string
-}
-
-interface GoogleLoginValue {
-    state: string;
-    access_token: string;
-    token_type: "Bearer" | string;
-    expires_in: string; // seconds
-    scope: string; //e.g. email https://www.googleapis.com/auth/userinfo.email openid
-    authuser: string; //numeric string
-    prompt: string;
-    session_state: string;
-};
-
-function getOAuthObjectFromSearchString(searchString: string): GoogleLoginValue {
-    return JSON.parse(decodeURI(searchString.replace(/^\?/, "")));
-}
-
-function LoginView(props: { searchString: string }) {
-
-    const [userInfo, setUserInfo] = React.useState<GoogleUserInfo>();
-
-    const oAuthObjectFromSearchString = getOAuthObjectFromSearchString(props.searchString);
-
-
-    if (!userInfo)
-        fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
-            method: "GET",
-            headers: {
-                "Accept": "application/json",
-                "Authorization": "Bearer " + oAuthObjectFromSearchString.access_token
-            }
-        }).then((response) => {
-            if (response.status !== 200) {
-                throw new Error("Unexpected response status: " + response.status);
-            }
-            return response.json();
-        }).then((value: GoogleUserInfo) => setUserInfo(value));
-
-    const div = <div>
-        <label>oAuthObject
-            <p>{JSON.stringify(oAuthObjectFromSearchString)}</p>
-        </label>
-        <label>userString
-            <p>{JSON.stringify(userInfo)}</p>
-        </label>
-    </div>;
-    return div;
-}
+import {LoginView} from "./LoginView";
 
 export default function App() {
-
     return (
         <main>
             <UserView/>
@@ -69,7 +15,7 @@ export default function App() {
             <Route render={props => {
                 props.history.listen(() => window.location.reload());
                 return <Switch>
-                    <Route path="/login/google"
+                    <Route exact path="/login/*"
                            render={routeProps => <LoginView searchString={routeProps.location.search}/>}/>
                     <Route exact path="/">
                         <Redirect to={"/days"}/>
