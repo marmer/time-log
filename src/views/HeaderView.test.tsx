c8959b85fa72d12171b4ff7857d735a7d93515c0
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-13 15:21:05 +0100

    - some header added

---
+++ src/views/HeaderView.test.tsx
@@ -0,0 +1,20 @@
+import HeaderView from "./HeaderView";
+import * as reactTest from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import React from "react";
+
+jest.mock("./UserView", () => (): React.ReactNode => <div>UserView</div>);
+
+describe("HeaderView", () => {
+    it("should show the project name somewhere", async () => {
+        const underTest = reactTest.render(<HeaderView/>);
+
+        expect(underTest.getByText("time-log")).toBeVisible()
+    });
+
+    it("should show the user view", async () => {
+        const underTest = reactTest.render(<HeaderView/>);
+
+        expect(underTest.getByText("UserView")).toBeVisible()
+    });
+});
--- src/views/HeaderView.tsx
+++ src/views/HeaderView.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserView from "./UserView";
+import projectConfiguration from "../../package.json"
 
-export default () => <div>
+export default () => <header>
+    <h1>{projectConfiguration.name}</h1>
     <UserView/>
-</div>;
+</header>;
 
 
