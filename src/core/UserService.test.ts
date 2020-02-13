a2f4a0541a168d4d3208d29858f7f84da6d7df8e
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-13 18:18:11 +0100

    - some hook to go on

---
+++ src/core/UserService.test.ts
@@ -0,0 +1,8 @@
+import UserService from "./UserService";
+
+describe("UserService", () => {
+    describe("performLogin", () => {
+        // TODO: marmer 13.02.2020 what about some environment variables ;)
+        fail("Go on here!!!")
+    });
+});
