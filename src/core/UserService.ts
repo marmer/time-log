93791c53d124a71e92d36a325ea679bf6bb64d5c
marmer <Mariano.Mertinat@gmail.com>
2020-02-14 15:39:46 +0100

    -  Secrets removed again

--- src/core/UserService.ts
+++ src/core/UserService.ts
@@ -15,9 +15,6 @@ export default class UserService {
         if (!process.env.REACT_APP_OAUTH_CLIENT_ID)
             result.push("REACT_APP_OAUTH_CLIENT_ID");
 
-        if (!process.env.REACT_APP_OAUTH_CLIENT_SECRET)
-            result.push("REACT_APP_OAUTH_CLIENT_SECRET");
-
         if (!process.env.REACT_APP_OAUTH_REDIRECT_URL)
             result.push("REACT_APP_OAUTH_REDIRECT_URL");
 
