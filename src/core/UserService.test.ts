5decef98c550ee88ac8d8d344c0bdf08874b3ab5
marmer <Mariano.Mertinat@gmail.com>
2020-02-14 09:51:47 +0100

    - Hook removed temporarily

--- src/core/UserService.test.ts
+++
@@ -1,8 +0,0 @@
-import UserService from "./UserService";
-
-describe("UserService", () => {
-    describe("performLogin", () => {
-        // TODO: marmer 13.02.2020 what about some environment variables ;)
-        fail("Go on here!!!")
-    });
-});
