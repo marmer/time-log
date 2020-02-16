1ee3b2340ca69dad2cb90772715c838b0bd33503
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-16 18:36:52 +0100

    - error handling for fetching of user infos

--- src/google/oauth/GoogleUserInfoCrudService.test.ts
+++ src/google/oauth/GoogleUserInfoCrudService.test.ts
@@ -2,6 +2,12 @@ import GoogleUserInfoCrudService from "./GoogleUserInfoCrudService";
 import fetchMock from "fetch-mock";
 
 describe("GoogleUserInfoCrudService", () => {
+
+    beforeEach(() => {
+        fetchMock.reset();
+        jest.resetAllMocks();
+    });
+
     describe("getUserInfo", () => {
         describe("server responds successful", () => {
             it("should resolve with an appropriate value", async () => {
@@ -15,6 +21,7 @@ describe("GoogleUserInfoCrudService", () => {
                         "Authorization": "Bearer " + accessToken
                     }
                 }, {
+                    status: 200,
                     body: "{\n  \"id\": \"someId\",\n  \"email\": \"some@mail\",\n  \"verified_email\": true,\n  \"picture\": \"http://some.url\"\n}"
                 });
 
@@ -22,7 +29,25 @@ describe("GoogleUserInfoCrudService", () => {
             });
         });
 
-        // TODO: marmer 16.02.2020 bad status code
+        describe("response with wrong status code", () => {
+            it("should reject with an appropriate error message", async () => {
+                const accessToken = "someToken";
+
+                fetchMock.mock({
+                    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+                    method: "GET",
+                    headers: {
+                        "Accept": "application/json",
+                        "Authorization": "Bearer " + accessToken
+                    }
+                }, {
+                    status: 401,
+                    body: "{\n  \"id\": \"someId\",\n  \"email\": \"some@mail\",\n  \"verified_email\": true,\n  \"picture\": \"http://some.url\"\n}"
+                });
+
+                await expect(GoogleUserInfoCrudService.getUserInfo(accessToken)).rejects.toStrictEqual(new Error("Cannot get user infos. Request for User infos end with status code 401"));
+            });
+        });
 
     });
 });
