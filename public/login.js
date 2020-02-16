2330e2c3657d4e7b71a86f2d5490c9be59aeb82f
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-16 19:49:42 +0100

    - untested logic replaced by a simple redirect

--- public/login.js
+++ public/login.js
@@ -1,12 +1,3 @@
-const requestObject = decodeURI(window.location.hash)
-    .replace(/^#/, "")
-    .split("&")
-    .map(value => value.split("="))
-    .map(kv => {
-        const retVal = {};
-        retVal[kv[0]] = kv[1];
-        return retVal;
-    })
-    .reduce((previousValue, currentValue) => ({...previousValue, ...currentValue}));
-
-window.location.href = "./#/login/google?" + JSON.stringify(requestObject);
+window.location.href = "./#/login/google?" +
+    window.location.hash
+        .replace(/^#/, "");
