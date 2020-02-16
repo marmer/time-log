3e14ef154b3199f9cb3820a9cc170f4d6fd86f09
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-16 14:05:38 +0100

    - some dead blocks with placeholder for upcoming tests - fetch-mock added for rest tests

---
+++ src/core/LoginService.test.ts
@@ -0,0 +1,30 @@
+import LoginService from "./LoginService";
+import fetchMock from "fetch-mock";
+
+describe("LoginService", () => {
+    beforeEach(() => {
+        fetchMock.reset();
+        jest.resetAllMocks();
+    });
+
+    describe("LoginBySearchString", () => {
+
+        describe("valid search string", () => {
+            describe("error search string", () => {
+                // TODO: marmer 16.02.2020
+            });
+            describe("success search string", () => {
+
+                describe("valid response", () => {
+                    // TODO: marmer 16.02.2020
+                });
+                describe("error response", () => {
+                    // TODO: marmer 16.02.2020
+                });
+            });
+        });
+        describe("invalid search string", () => {
+            // TODO: marmer 16.02.2020
+        });
+    });
+});
