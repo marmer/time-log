e8ec09a76a0cc4e11fb2ef6367ce09cd56246e3b
marmer <Mariano.Mertinat@gmail.com>
2020-02-16 18:26:55 +0100

    - fetching of user infos

--- src/google/oauth/GoogleUserInfoCrudService.ts
+++ src/google/oauth/GoogleUserInfoCrudService.ts
@@ -1,4 +1,8 @@
 export interface GoogleUserInfo {
+    email: string;
+}
+
+export interface GoogleUserDto {
     id: string;
     email: string;
     verified_email: boolean;
@@ -8,7 +12,28 @@ export interface GoogleUserInfo {
 export default class GoogleUserInfoCrudService {
 
     static async getUserInfo(accessToken: string): Promise<GoogleUserInfo> {
-        // TODO: marmer 16.02.2020
-        return Promise.reject("ya ya ya ya ya ;)");
+        return fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + accessToken
+            }
+        })
+            .then(response => response.json())
+            .then((dto: GoogleUserDto) => ({email: dto.email} as GoogleUserInfo))
+
+
+        // .the
+        //
+        // response.json()
+        //
+        // .then((response) => {
+        // if (response.status !== 200) {
+        //     throw new Error("Unexpected response status: " + response.status);
+        // }
+        // return response.json();
+        // });
+        //
+        // response.
     }
 }
