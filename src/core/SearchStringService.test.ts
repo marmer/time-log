123eb4f387718ba593ba183a09c42469d481c7f4
marmer <Mariano.Mertinat@gmail.com>
2020-02-16 17:37:51 +0100

    - parsing of search strings

--- src/core/SearchStringService.test.ts
+++ src/core/SearchStringService.test.ts
@@ -26,10 +26,42 @@ describe("SearchStringService", () => {
                 });
             });
 
-            // TODO: marmer 16.02.2020 malformed uri component
-            // TODO: marmer 16.02.2020 malformed search String (no ? at the beginning)
-            // TODO: marmer 16.02.2020 not value for one of the parameters
-            // TODO: marmer 16.02.2020 empty search string
+            describe("parameter values are URL encoded", () => {
+                it("should decoded withing the result", async () => {
+                    const result = SearchStringService.parse("?k1=a%20B,C%25D:e?f/g%5Ch");
+                    expect(result).toStrictEqual({
+                        k1: "a B,C%D:e?f/g\\h"
+                    });
+
+                });
+            });
+
+            describe("The given string does not start with a questionmark", () => {
+                it("it should be assumed the given string is a search string just without a questionmark ", async () => {
+                    const result = SearchStringService.parse("k1=v1");
+                    expect(result).toStrictEqual({
+                        k1: "v1"
+                    });
+                });
+            });
+
+            describe("parameter has an empty value assignment", () => {
+                it("should be an empty result for the value", async () => {
+                    const result = SearchStringService.parse("k1=");
+                    expect(result).toStrictEqual({
+                        k1: ""
+                    });
+                });
+            });
+
+            describe("parameter without any assignment", () => {
+                it("should be an empty result for the value", async () => {
+                    const result = SearchStringService.parse("k1");
+                    expect(result).toStrictEqual({
+                        k1: ""
+                    });
+                });
+            });
         });
     });
 });
