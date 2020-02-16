3b74f58c71ff009671c5b6db1d740d0c710e4df5
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-16 17:37:51 +0100

    - parsing of search strings

--- src/core/SearchStringService.ts
+++ src/core/SearchStringService.ts
@@ -1,15 +1,12 @@
 export default class SearchStringService {
 
     static parse<T>(searchString: string): T {
-        const searchStringWithoutTheQuestionmark = this.removeSearchStringOpening(searchString);
-
-        const kvPairs: string[] = searchStringWithoutTheQuestionmark.split("&");
-
-        return kvPairs
+        return this.removeSearchStringOpening(searchString)
+            .split("&")
             .filter(kvPairs => kvPairs[0])
             .map(kvPair => kvPair.split("="))
             .reduce((result, kvPair) => {
-                    result[kvPair[0]] = kvPair[1];
+                    result[kvPair[0]] = kvPair[1] ? decodeURIComponent(kvPair[1]) : "";
                     return result;
                 }
                 , {} as { [key: string]: string }) as any;
