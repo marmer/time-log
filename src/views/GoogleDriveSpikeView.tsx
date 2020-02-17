c44327b3bb521e49db4f2e4c41af8a0ed281bf64
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-17 17:50:10 +0100

    - Spike of Loading of list metadata of files

--- src/views/GoogleDriveSpikeView.tsx
+++ src/views/GoogleDriveSpikeView.tsx
@@ -1,6 +1,76 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import UserService from "../core/UserService";
+
+interface File {
+    id: string,
+    name: string,
+    content?: string;
+}
+
+interface FileListResponseEntryDto {
+    "id": string
+    "name": string,
+    "mimeType": string
+}
+
+interface GoogleFileListResponseDto {
+    "files": FileListResponseEntryDto[]
+}
+
+function FileRespoinseView(props: { files: { [id: string]: File } }) {
+    return <div>
+        <table>
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>name</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                {Object.keys(props.files)
+                    .map(key => props.files[key])
+                    .map(file =>
+                        <tr>
+                            <td>{file.id}</td>
+                            <td>{file.name}</td>
+                        </tr>)}
+            </tr>
+            </tbody>
+        </table>
+    </div>
+}
 
 export default () => {
-    return <div>some Spike</div>
+    const [driveFiles, setFiles] = useState<{ [id: string]: File }>({});
+
+    useEffect(() => {
+        if (UserService.getCurrentUser())
+            fetch("https://www.googleapis.com/drive/v3/files?=", {
+                "method": "GET",
+                "headers": {
+                    "accept": "application/json",
+                    "authorization": "Bearer " + UserService.getCurrentUser()?.accessToken
+                }
+            })
+                .then(response => {
+                    if (response.status !== 200) throw new Error("bad status code");
+                    return response.json();
+                })
+                .then(response => {
+                    return response as GoogleFileListResponseDto
+                })
+                .then(fileList => {
+                    setFiles(fileList.files
+                        .map(({id, name}) => ({[id]: ({id, name} as File)}))
+                        .reduce((f1, f2) => ({...f1, ...f2})));
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+    }, [driveFiles.length]);
+
+    return <div><label>File List of google drive files List metadata<FileRespoinseView files={driveFiles}/></label>
+    </div>
 }
 
