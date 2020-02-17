47899a76bb78897f2db055736230904861236e28
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-17 18:16:52 +0100

    - Spike of Loading file contents for files

--- src/views/GoogleDriveSpikeView.tsx
+++ src/views/GoogleDriveSpikeView.tsx
@@ -24,6 +24,7 @@ function FileRespoinseView(props: { files: { [id: string]: File } }) {
             <tr>
                 <th>id</th>
                 <th>name</th>
+                <th>content</th>
             </tr>
             </thead>
             <tbody>
@@ -34,6 +35,7 @@ function FileRespoinseView(props: { files: { [id: string]: File } }) {
                         <tr>
                             <td>{file.id}</td>
                             <td>{file.name}</td>
+                            <td>{file.content}</td>
                         </tr>)}
             </tr>
             </tbody>
@@ -41,33 +43,81 @@ function FileRespoinseView(props: { files: { [id: string]: File } }) {
     </div>
 }
 
+function addFileMetadataFor(file: File, setFiles: (value: (((prevState: { [p: string]: File }) => { [p: string]: File }) | { [p: string]: File })) => void) {
+    fetch(`https://www.googleapis.com/drive/v3/files/${file.id}`, {
+        "method": "GET",
+        "headers": {
+            "authorization": "Bearer " + UserService.getCurrentUser()?.accessToken
+        }
+    })
+        .then(response => {
+            response.json()
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    ;
+}
+
+function loadFiles(setDriveFiles: (value: (((prevState: { [p: string]: File }) => { [p: string]: File }) | { [p: string]: File })) => void) {
+    if (UserService.getCurrentUser())
+        fetch("https://www.googleapis.com/drive/v3/files?=", {
+            "method": "GET",
+            "headers": {
+                "accept": "application/json",
+                "authorization": "Bearer " + UserService.getCurrentUser()?.accessToken
+            }
+        })
+            .then(response => {
+                if (response.status !== 200) throw new Error("bad status code");
+                return response.json();
+            })
+            .then(response => {
+                return response as GoogleFileListResponseDto
+            })
+            .then(fileList => {
+                const driveFiles = fileList.files
+                    .map(({id, name}) => ({[id]: {id, name} as File}))
+                    .reduce((f1, f2) => ({...f1, ...f2}));
+                setDriveFiles(driveFiles);
+
+                loadFileContentFor(driveFiles, setDriveFiles);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+}
+
+function loadFileContentFor(driveFiles: { [id: string]: File }, setDriveFiles: (value: (((prevState: { [p: string]: File }) => { [p: string]: File }) | { [p: string]: File })) => void) {
+    if (UserService.getCurrentUser())
+        Object.keys(driveFiles)
+            .forEach(id => {
+                fetch(`https://www.googleapis.com/drive/v3/files/${id}?alt=media`, {
+                    "method": "GET",
+                    "headers": {
+                        "authorization": `Bearer ${UserService.getCurrentUser()?.accessToken}`
+                    }
+                })
+                    .then(response => {
+                        console.log(response);
+                        return response.text()
+                    })
+                    .then(fileContent => {
+                        const df = {...driveFiles};
+                        df[id].content = fileContent;
+                        setDriveFiles(df)
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            });
+}
+
 export default () => {
-    const [driveFiles, setFiles] = useState<{ [id: string]: File }>({});
+    const [driveFiles, setDriveFiles] = useState<{ [id: string]: File }>({});
 
     useEffect(() => {
-        if (UserService.getCurrentUser())
-            fetch("https://www.googleapis.com/drive/v3/files?=", {
-                "method": "GET",
-                "headers": {
-                    "accept": "application/json",
-                    "authorization": "Bearer " + UserService.getCurrentUser()?.accessToken
-                }
-            })
-                .then(response => {
-                    if (response.status !== 200) throw new Error("bad status code");
-                    return response.json();
-                })
-                .then(response => {
-                    return response as GoogleFileListResponseDto
-                })
-                .then(fileList => {
-                    setFiles(fileList.files
-                        .map(({id, name}) => ({[id]: ({id, name} as File)}))
-                        .reduce((f1, f2) => ({...f1, ...f2})));
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+        loadFiles(setDriveFiles);
     }, [driveFiles.length]);
 
     return <div><label>File List of google drive files List metadata<FileRespoinseView files={driveFiles}/></label>
