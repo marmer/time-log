07fb3030f8e871e707362666d80f7e05b6c1ab42
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-22 16:44:14 +0100

    - weeks removed from jira string calculation

--- src/core/JiraTimeService.test.ts
+++ src/core/JiraTimeService.test.ts
@@ -8,13 +8,7 @@ describe("JiraTimeFormatter", () => {
             {timeSpentInMinutes: 59, expectedJiraString: "59m"},
             {timeSpentInMinutes: 60, expectedJiraString: "1h"},
             {timeSpentInMinutes: 479, expectedJiraString: "7h 59m"},
-            {timeSpentInMinutes: 480, expectedJiraString: "1d"},
-            {timeSpentInMinutes: 2399, expectedJiraString: "4d 7h 59m"},
-            {timeSpentInMinutes: 2400, expectedJiraString: "1w"},
-            {timeSpentInMinutes: 4799, expectedJiraString: "1w 4d 7h 59m"},
-            {timeSpentInMinutes: 4800, expectedJiraString: "2w"},
-            {timeSpentInMinutes: 2459, expectedJiraString: "1w 59m"},
-            {timeSpentInMinutes: -4799, expectedJiraString: "-1w 4d 7h 59m"},
+            {timeSpentInMinutes: -479, expectedJiraString: "-7h 59m"},
         ].forEach(parameter => {
             it("should split transform " + parameter.timeSpentInMinutes + ' into the jira string "' + parameter.expectedJiraString + '"', () => {
                 expect(JiraTimeFormatter.minutesToJiraFormat(parameter.timeSpentInMinutes)).toBe(parameter.expectedJiraString);
@@ -36,14 +30,9 @@ describe("JiraTimeFormatter", () => {
             {input: "1h", isJiraFormat: true},
             {input: "1h 15", isJiraFormat: true},
             {input: "-1h 15", isJiraFormat: true},
-            {input: "1d", isJiraFormat: true},
-            {input: "1w", isJiraFormat: true},
-            {input: " 1h 1m 1w 1d", isJiraFormat: true},
-            {input: "   1h \t 1m   1w   1d   ", isJiraFormat: true},
             {input: "bla", isJiraFormat: false},
             {input: "bla", isJiraFormat: false},
             {input: "8823m", isJiraFormat: true},
-            {input: " 1h 1mimimi 1w 1d", isJiraFormat: false},
 
         ].forEach(parameter => {
             it("should " + parameter.input + " be valid? ===" + parameter.isJiraFormat, () => {
@@ -75,12 +64,7 @@ describe("JiraTimeFormatter", () => {
             {jiraString: "15 1h", expectedTimeSpentInMinutes: 75},
             {jiraString: "1h 5m 2h", expectedTimeSpentInMinutes: 185},
             {jiraString: "7h 59m", expectedTimeSpentInMinutes: 479},
-            {jiraString: "1d", expectedTimeSpentInMinutes: 480},
-            {jiraString: "4d 7h 59m", expectedTimeSpentInMinutes: 2399},
-            {jiraString: "1w", expectedTimeSpentInMinutes: 2400},
-            {jiraString: "1w 4d 7h 59m", expectedTimeSpentInMinutes: 4799},
-            {jiraString: "2w", expectedTimeSpentInMinutes: 4800},
-            {jiraString: "1w 59m", expectedTimeSpentInMinutes: 2459},
+            {jiraString: "8h 5m", expectedTimeSpentInMinutes: 485},
         ].forEach(parameter => {
             it('should transform the jira String "' + parameter.jiraString + '" into its numeric value in minutes "' + parameter.expectedTimeSpentInMinutes + '"', () => {
                 expect(JiraTimeFormatter.jiraFormatToMinutes(parameter.jiraString)).toBe(parameter.expectedTimeSpentInMinutes);
