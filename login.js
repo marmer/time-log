87a913b4cefbf9a398fa09cc8e51ec02dc8ebd17
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-22 16:43:56 +0000

    Deploying to gh-pages from  - 57e45df484b497b783729cb083462cf1a7238d45 🚀

---
+++ login.js
@@ -0,0 +1,3 @@
+window.location.href = "./login/google?" +
+    window.location.hash
+        .replace(/^#/, "");
---
+++ precache-manifest.ee604336629eadbcc0f392fb942b8a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f89ab659222dcd7777c93c3c4a60ae",
+    "url": "/time-log/index.html"
+  },
+  {
+    "revision": "0993db5b8a4463c99d10",
+    "url": "/time-log/static/css/2.08c74b9a.chunk.css"
+  },
+  {
+    "revision": "ebe5d695a0a9c8cc0ebc",
+    "url": "/time-log/static/css/main.895ffa39.chunk.css"
+  },
+  {
+    "revision": "0993db5b8a4463c99d10",
+    "url": "/time-log/static/js/2.0990bcd5.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/time-log/static/js/2.0990bcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe5d695a0a9c8cc0ebc",
+    "url": "/time-log/static/js/main.cef4646a.chunk.js"
+  },
+  {
+    "revision": "8bba61cab2cc2ca10a76",
+    "url": "/time-log/static/js/runtime-main.298d46d2.js"
+  }
+]);
