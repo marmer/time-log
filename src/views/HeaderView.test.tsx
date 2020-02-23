425abfe03798481e60ec4df622ee399cb5088511
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-23 17:28:43 +0100

    - A little navigation

--- src/views/HeaderView.test.tsx
+++ src/views/HeaderView.test.tsx
@@ -2,14 +2,45 @@ import HeaderView from "./HeaderView";
 import * as reactTest from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import React from "react";
+import {MemoryRouter, Router} from "react-router-dom";
+import {createMemoryHistory} from 'history'
 
 jest.mock("./UserView", () => (): React.ReactNode => <div>UserView</div>);
 
 describe("HeaderView", () => {
     it("should show the project name somewhere", async () => {
-        const underTest = reactTest.render(<HeaderView/>);
+        const underTest = reactTest.render(<MemoryRouter><HeaderView/></MemoryRouter>);
 
-        expect(underTest.getByText("time-log")).toBeVisible()
+        expect(underTest.getByText("time-log")).toBeVisible();
     });
 
+    describe("Navigation highlights", () => {
+        it("should mark the settings location depending on the window location", async () => {
+            const history = createMemoryHistory({
+                initialEntries: ["/somewhere"]
+            });
+
+            const underTest = reactTest.render(<Router history={history}><HeaderView/></Router>);
+
+            expect(underTest.getByText("Settings").classList).not.toContain("active");
+            history.push("/settings");
+            expect(underTest.getByText("Settings").classList).toContain("active");
+        });
+
+        it("should mark the day location depending on the window location", async () => {
+            const history = createMemoryHistory({
+                initialEntries: ["/somewhere"]
+            });
+
+            const underTest = reactTest.render(<Router history={history}><HeaderView/></Router>);
+
+            expect(underTest.getByText("Logs").classList).not.toContain("active");
+            history.push("/day");
+            expect(underTest.getByText("Logs").classList).toContain("active");
+            history.push("/day/someDay");
+            expect(underTest.getByText("Logs").classList).toContain("active");
+            history.push("/somewhereElse");
+            expect(underTest.getByText("Logs").classList).not.toContain("active");
+        });
+    });
 });
--- src/views/HeaderView.tsx
+++ src/views/HeaderView.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import projectConfiguration from "../../package.json"
+import {Link, NavLink} from "react-router-dom";
 
 export default () => <header>
     <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand mb-0 h1">{projectConfiguration.name}</span>
+        <Link to={"/days/today"} className="navbar-brand">{projectConfiguration.name}</Link>
+        <div className="navbar-expand">
+            <div className="navbar-nav">
+                <NavLink to={"/days/today"}
+                         isActive={(match, location) => location.pathname.startsWith("/day")}
+                         className="nav-item nav-link"
+                         activeClassName="active">Logs</NavLink>
+                <NavLink to={"/settings"} className="nav-item nav-link" activeClassName="active">Settings</NavLink>
+            </div>
+        </div>
     </nav>
-
 </header>;
 
 
