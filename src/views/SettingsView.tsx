79ea696b57fd01558873b6044248610cf1ecaeb8
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-23 18:16:43 +0100

    - mockup for settings

--- src/views/SettingsView.tsx
+++ src/views/SettingsView.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default () =>
-    <div>Settings View</div>
-
+export default () => {
+    return <form onSubmit={_ => false}>
+        <div className="card">
+            <div className="card-header">
+                Day Settings
+            </div>
+            <div className="form-group row card-body">
+                <label htmlFor="expectedTimeToLog">Expected Time to log per day in Minutes</label>
+                <input id="expectedTimeToLog" type="number" className="form-control"
+                       placeholder="e.g. 480 for 8 hours"/>
+            </div>
+        </div>
+    </form>
+}
