a4ff46799707132b7371aeb930ce2f1bc4a3df4d
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-23 09:43:54 +0100

    - some Navbar styling

--- src/views/HeaderView.tsx
+++ src/views/HeaderView.tsx
@@ -3,7 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import projectConfiguration from "../../package.json"
 
 export default () => <header>
-    <h1>{projectConfiguration.name}</h1>
+    <nav className="navbar navbar-dark bg-dark">
+        <span className="navbar-brand mb-0 h1">{projectConfiguration.name}</span>
+    </nav>
 
 </header>;
 
