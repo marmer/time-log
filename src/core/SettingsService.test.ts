3a0341f7a6390c8012cfb9a276225881c2fd9cc8
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-23 21:21:17 +0100

    - useless parameter removed

--- src/core/SettingsService.test.ts
+++ src/core/SettingsService.test.ts
@@ -5,16 +5,12 @@ describe("SettingsService", () => {
     it("should resolve with a default value of 8 hours if nothing is stored in the repository yet", async () => {
         SettingsRepository.getExpectedDailyTimelogInMinutes = jest.fn().mockReturnValue(null);
 
-        const anyDate = new Date();
-
-        return expect(SettingsService.getExpectedDailyTimelogInMinutes(anyDate)).resolves.toBe(480);
+        return expect(SettingsService.getExpectedDailyTimelogInMinutes()).resolves.toBe(480);
     });
 
     it("should return the value of the repository if it serves a value", async () => {
         SettingsRepository.getExpectedDailyTimelogInMinutes = jest.fn().mockReturnValue(42);
 
-        const anyDate = new Date();
-
-        return expect(SettingsService.getExpectedDailyTimelogInMinutes(anyDate)).resolves.toBe(42);
+        return expect(SettingsService.getExpectedDailyTimelogInMinutes()).resolves.toBe(42);
     });
 });
