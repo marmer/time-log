d8f77ce111075c3bb1ae8f8c086d22f52e7f6695
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-23 17:46:25 +0100

    - some logo

--- src/views/HeaderView.tsx
+++ src/views/HeaderView.tsx
@@ -5,7 +5,10 @@ import {Link, NavLink} from "react-router-dom";
 
 export default () => <header>
     <nav className="navbar navbar-dark bg-dark">
-        <Link to={"/days/today"} className="navbar-brand">{projectConfiguration.name}</Link>
+        <Link to={"/days/today"} className="navbar-brand">
+            <img src="/logo192.png" width="30" height="30"
+                 className="d-inline-block align-top" alt="logo">
+            </img> {projectConfiguration.name}</Link>
         <div className="navbar-expand">
             <div className="navbar-nav">
                 <NavLink to={"/days/today"}
