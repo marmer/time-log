5c084d244ed046823fd39564c64e61eec4f473d7
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-23 20:40:10 +0100

    - Loading of expected daily time to log within the settings view

--- src/views/SettingsView.test.tsx
+++ src/views/SettingsView.test.tsx
@@ -6,6 +6,7 @@ import SettingsView from "./SettingsView";
 import SettingsService from "../core/SettingsService";
 
 describe("SettingsView", () => {
+
     it("should show some loading state while loading the settings", async () => {
         SettingsService.getExpectedDailyTimelogInMinutes = jest.fn().mockResolvedValue({expectedDailyTimelogInMinutes: 480});
 
@@ -13,18 +14,24 @@ describe("SettingsView", () => {
 
         const dailyTimelogField = underTest.getByLabelText("Expected Time to log per day");
         expect(dailyTimelogField).toBeDisabled();
-        expect(dailyTimelogField).toHaveProperty("placeholder", "loading...");
+        expect(dailyTimelogField).toHaveValue("Loading...");
         expect(underTest.getByTitle("save")).toBeDisabled()
     });
 
     it("should show tie current expected daily time to log after loading", async () => {
-        SettingsService.getExpectedDailyTimelogInMinutes = jest.fn().mockResolvedValue({expectedDailyTimelogInMinutes: 480});
+        SettingsService.getExpectedDailyTimelogInMinutes = jest.fn().mockResolvedValue(480);
 
         const underTest = reactTest.render(<SettingsView/>);
+
         const dailyTimelogField = underTest.getByLabelText("Expected Time to log per day");
+        console.log(reactTest.prettyDOM(dailyTimelogField));
         await reactTest.waitForDomChange({container: dailyTimelogField});
+        console.log(reactTest.prettyDOM(dailyTimelogField));
         expect(dailyTimelogField).toBeEnabled();
         expect(dailyTimelogField).toHaveValue("8h");
         expect(underTest.getByTitle("save")).toBeEnabled()
     });
+    // TODO: marmer 23.02.2020 save
+    // TODO: marmer 23.02.2020 invalid values
+    // TODO: marmer 23.02.2020 Errorhandling while loading
 });
--- src/views/SettingsView.tsx
+++ src/views/SettingsView.tsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import SettingsService from "../core/SettingsService";
+import JiraTimeService from "../core/JiraTimeService";
 
+const loadingPlaceholder = "Loading...";
 export default () => {
-    return <form onSubmit={_ => false}>
-        <div className="card">
-            <div className="card-header">
-                Day Settings
-            </div>
-            <div className="form-group row card-body">
+    const [expectedDailyTimelogInMinutes, setExpectedDailyTimelogInMinutes] = useState<{
+        value: string,
+        isLoading: boolean
+    }>({value: loadingPlaceholder, isLoading: true});
+
+    useEffect(() => {
+        setExpectedDailyTimelogInMinutes({
+            isLoading: true,
+            value: loadingPlaceholder
+        });
+        SettingsService.getExpectedDailyTimelogInMinutes()
+            .then(minutes => {
+                setExpectedDailyTimelogInMinutes({
+                    isLoading: false,
+                    value: JiraTimeService.minutesToJiraFormat(minutes)
+                });
+            })
+    }, []);
+
+    return <div className="card">
+        <div className="card-header">
+            Day Settings
+        </div>
+        <div className="form-group row card-body">
+            <form onSubmit={_ => false}>
                 <label htmlFor="expectedTimeToLog">Expected Time to log per day</label>
-                <input id="expectedTimeToLog" type="number" className="form-control"
+                <input id="expectedTimeToLog" type="text" className="form-control"
+                       disabled={expectedDailyTimelogInMinutes.isLoading}
+                       value={expectedDailyTimelogInMinutes.value}
+                       onChange={({target}) => setExpectedDailyTimelogInMinutes({
+                           ...expectedDailyTimelogInMinutes, value: target.value
+                       })}
                        placeholder="e.g. 7h 30m"/>
+                <button className="btn btn-primary fullWidth" title="save"
+                        disabled={!JiraTimeService.isValidJiraFormat(expectedDailyTimelogInMinutes.value)}
+                        type={"submit"}
+                ><i className="fa fa-save"/> save
+                </button>
+            </form>
             </div>
         </div>
-        <button className="btn btn-primary fullWidth" title="save"
-                type={"submit"}
-        ><i className="fa fa-save"/> save
-        </button>
-    </form>
+
 }
