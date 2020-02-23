f8c05bd988851052db614a4ff79582265e255df8
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-23 20:49:10 +0100

    - a little more description

--- src/views/SettingsView.test.tsx
+++ src/views/SettingsView.test.tsx
@@ -6,17 +6,18 @@ import SettingsView from "./SettingsView";
 import SettingsService from "../core/SettingsService";
 
 describe("SettingsView", () => {
+    describe("getExpectedDailyTimelogInMinutes", () => {
 
-    it("should show some loading state while loading the settings", async () => {
-        SettingsService.getExpectedDailyTimelogInMinutes = jest.fn().mockResolvedValue({expectedDailyTimelogInMinutes: 480});
+        it("should show some loading state while loading the settings", async () => {
+            SettingsService.getExpectedDailyTimelogInMinutes = jest.fn().mockResolvedValue({expectedDailyTimelogInMinutes: 480});
 
-        const underTest = reactTest.render(<SettingsView/>);
+            const underTest = reactTest.render(<SettingsView/>);
 
-        const dailyTimelogField = underTest.getByLabelText("Expected Time to log per day");
-        expect(dailyTimelogField).toBeDisabled();
-        expect(dailyTimelogField).toHaveValue("Loading...");
-        expect(underTest.getByTitle("save")).toBeDisabled()
-    });
+            const dailyTimelogField = underTest.getByLabelText("Expected Time to log per day");
+            expect(dailyTimelogField).toBeDisabled();
+            expect(dailyTimelogField).toHaveValue("Loading...");
+            expect(underTest.getByTitle("save")).toBeDisabled()
+        });
 
     it("should show tie current expected daily time to log after loading", async () => {
         SettingsService.getExpectedDailyTimelogInMinutes = jest.fn().mockResolvedValue(480);
@@ -29,7 +30,8 @@ describe("SettingsView", () => {
         expect(dailyTimelogField).toHaveValue("8h");
         expect(underTest.getByTitle("save")).toBeEnabled()
     });
-    // TODO: marmer 23.02.2020 save
-    // TODO: marmer 23.02.2020 invalid values
-    // TODO: marmer 23.02.2020 Errorhandling while loading
+        // TODO: marmer 23.02.2020 save
+        // TODO: marmer 23.02.2020 invalid values
+        // TODO: marmer 23.02.2020 Errorhandling while loading
+    });
 });
