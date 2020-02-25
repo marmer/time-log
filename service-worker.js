69449005692a4d4a74d9b5228c26dd7af13164c8
MarMer <1055445+marmer@users.noreply.github.com>
2020-02-25 14:35:16 +0000

    Deploying to gh-pages from  - dd75eda0ab106a1bcd846d410992fba674efd044 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/time-log/precache-manifest.65c8cb01dcdd8803bba773c760b4ffc8.js"
+  "/time-log/precache-manifest.77bf3e2fed3869cd932d2d32ec039ccc.js"
 );
 
 self.addEventListener('message', (event) => {
