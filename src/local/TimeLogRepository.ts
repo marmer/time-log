71aafb15e1607d98ba5233a1ef435ff79d8106e4
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-02-25 19:03:09 +0100

    Timelogs are stored in an indexedDB now

--- src/local/TimeLogRepository.ts
+++ src/local/TimeLogRepository.ts
@@ -1,31 +1,18 @@
 import {TimeLog} from "../core/TimeLogService";
-import moment from "moment";
 import TimeLogDatabase from "./TimeLogDatabase";
 
-interface TimeLogDbo {
-    [day: string]: TimeLog[]
-}
-
 export default class TimeLogRepository {
-    private static readonly STORE_KEY = "TimeLog";
-
     private static readonly db = new TimeLogDatabase();
 
-    public static async saveTimelogs(date: Date, timeLogs: TimeLog[]): Promise<TimeLog[]> {
-        return Promise.resolve([]);
+    public static async saveTimelogs(day: Date, timelogs: TimeLog[]): Promise<TimeLog[]> {
+        return TimeLogRepository.db.timelogDay.put({
+            day,
+            timelogs
+        }).then(() => timelogs);
     }
 
     public static async getTimeLogsForDay(date: Date): Promise<TimeLog[]> {
         const timelogDay = await TimeLogRepository.db.timelogDay.get(date);
-        if (!timelogDay) return [];
-        return timelogDay?.timelogs!;
-    }
-
-    private static getTimelogKeyForMonthOf(date: Date) {
-        return TimeLogRepository.STORE_KEY + moment(date).format("-YYYY-MM");
-    }
-
-    private static timelogKeyFor(date: Date) {
-        return moment(date).format("YYYY-MM-DD");
+        return timelogDay ? timelogDay.timelogs : [];
     }
 }
