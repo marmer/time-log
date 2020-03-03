0876767b40e48a03198aa38b39282ffa73275e00
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-03-03 14:22:42 +0100

    - Refactoring

--- src/views/DailyTimelogSettingsView.test.tsx
+++ src/views/DailyTimelogSettingsView.test.tsx
@@ -3,23 +3,27 @@ import '@testing-library/jest-dom/extend-expect'
 import * as reactTest from "@testing-library/react";
 
 import SettingsView from "./SettingsView";
-import DailyTimeLogSettingsService from "../core/DailyTimeLogSettingsService";
+import DailyTimeLogSettingsService, {DailyTimelogSettings} from "../core/DailyTimeLogSettingsService";
 import userEvent from "@testing-library/user-event";
+import deepmerge from "deepmerge";
 
+const dailyTimelogSettingsBase: DailyTimelogSettings = {
+    expectedDailyTimelogInMinutes: 480,
+    expectedTimelogDays: {
+        sunday: false,
+        monday: true,
+        tuesday: true,
+        wednesday: true,
+        thursday: true,
+        friday: true,
+        saturday: false
+    }
+};
 describe("SettingsView", () => {
     it("should show some loading state while loading the settings", async () => {
-        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue({
-            expectedDailyTimelogInMinutes: 480,
-            expectedTimelogDays: {
-                sunday: false,
-                monday: true,
-                tuesday: true,
-                wednesday: true,
-                thursday: true,
-                friday: true,
-                saturday: false
-            }
-        });
+        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue(
+            dailyTimelogSettingsBase
+        );
 
         const underTest = reactTest.render(<SettingsView/>);
 
@@ -30,18 +34,9 @@ describe("SettingsView", () => {
     });
 
     it("should show the current expected daily time to log after loading", async () => {
-        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue({
-            expectedDailyTimelogInMinutes: 480,
-            expectedTimelogDays: {
-                sunday: false,
-                monday: true,
-                tuesday: true,
-                wednesday: true,
-                thursday: true,
-                friday: true,
-                saturday: false
-            }
-        });
+        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue(
+            deepmerge(dailyTimelogSettingsBase, {expectedDailyTimelogInMinutes: 480})
+        );
 
         const underTest = reactTest.render(<SettingsView/>);
 
@@ -53,18 +48,9 @@ describe("SettingsView", () => {
     });
 
     it("should show tie current expected daily time to log after loading", async () => {
-        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue({
-            expectedDailyTimelogInMinutes: 480,
-            expectedTimelogDays: {
-                sunday: false,
-                monday: true,
-                tuesday: true,
-                wednesday: true,
-                thursday: true,
-                friday: true,
-                saturday: false
-            }
-        });
+        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue(
+            deepmerge(dailyTimelogSettingsBase, {expectedDailyTimelogInMinutes: 480})
+        );
         DailyTimeLogSettingsService.setExpectedDailyTimelogSettings = jest.fn();
 
         const underTest = reactTest.render(<SettingsView/>);
@@ -74,33 +60,13 @@ describe("SettingsView", () => {
         userEvent.type(dailyTimelogField, "15m");
         userEvent.click(underTest.getByTitle("save"));
 
-        expect(DailyTimeLogSettingsService.setExpectedDailyTimelogSettings).toBeCalledWith({
-            expectedDailyTimelogInMinutes: 15,
-            expectedTimelogDays: {
-                sunday: false,
-                monday: true,
-                tuesday: true,
-                wednesday: true,
-                thursday: true,
-                friday: true,
-                saturday: false
-            }
-        });
+        expect(DailyTimeLogSettingsService.setExpectedDailyTimelogSettings).toBeCalledWith(
+            deepmerge(dailyTimelogSettingsBase, {expectedDailyTimelogInMinutes: 15}));
     });
 
     it("should mark invalid values and make saving of that invalid value impossible", async () => {
-        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue({
-            expectedDailyTimelogInMinutes: 480,
-            expectedTimelogDays: {
-                sunday: false,
-                monday: true,
-                tuesday: true,
-                wednesday: true,
-                thursday: true,
-                friday: true,
-                saturday: false
-            }
-        });
+        DailyTimeLogSettingsService.getExpectedDailyTimelogSettings = jest.fn().mockResolvedValue(
+            deepmerge(dailyTimelogSettingsBase, {expectedDailyTimelogInMinutes: 480}));
         DailyTimeLogSettingsService.setExpectedDailyTimelogSettings = jest.fn();
 
         const underTest = reactTest.render(<SettingsView/>);
