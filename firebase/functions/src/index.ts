23c1d1c86d87498b9a77f626852a5c65a16a03ab
MarMer <1055445+marmer@users.noreply.github.com>
2020-03-29 00:09:16 +0100

    - refreshing of tokens implemented as firebase functin

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,12 +1,39 @@
 import * as functions from 'firebase-functions';
+import * as express from "express";
+
+const unirest = require("unirest");
+
+const app = express();
+
+app.get("/v1/auth/token/google/token/refresh/:refreshToken", async (request, response) => {
+    response.set('Access-Control-Allow-Origin', 'https://marmer.github.io');
+
+    const req = unirest("POST", "https://oauth2.googleapis.com/token");
+
+    req.timeout(30000);
+    req.query({
+        "refresh_token": request.params.refreshToken,
+        "client_id": "502499757378-a54o9nthuijpls8ihifb37tltl76tvgj.apps.googleusercontent.com",
+        "grant_type": "refresh_token",
+        "client_secret": process.env.CLIENT_SECRET
+    });
+
+
+    req.end((res: any) => {
+        response.status(res.status ? res.status : 500);
+        if (res.error) {
+            response.send(JSON.stringify({
+                "error": "unexpected error",
+                "error_description": "unexpected error"
+            }))
+        } else {
+            response.send(res.body);
+        }
+    });
+});
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.set('Access-Control-Allow-Origin', 'https://marmer.github.io');
-
- // TODO: marmer 21.03.2020 serve refresh token by a given short time google token
-
- response.send(JSON.stringify(request.query));
-});
+export const timelogapi = functions.https.onRequest(app);
