6c6ec23cda5515ea3ce222b56ad89233465dbe9a
MarMer <1055445+marmer@users.noreply.github.com>
2020-03-30 23:49:50 +0200

    - ability added to refresh an access token by a refresh token

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -10,6 +10,8 @@ app.use(bodyParser.json());
 // TODO: marmer 29.03.2020 Do Not Commit this Secret!!!!!
 const clientSecret = set_client_secret_before_deployment;
 
+// TODO: marmer 30.03.2020 cleanup and remove duplications
+
 app.get("/v1/auth/code", (request, response) => {
     response.set('Access-Control-Allow-Origin', 'https://marmer.github.io');
 
@@ -32,6 +34,48 @@ app.get("/v1/auth/code", (request, response) => {
         "redirect_uri": !query.state || query.state.includes("localhost") ?
             "http://localhost:5001/time-log-e5c6b/us-central1/timelogapi/v1/login" :
             "https://us-central1-time-log-e5c6b.cloudfunctions.net/timelogapi/v1/login",
+        "client_secret": clientSecret
+    });
+
+    req.headers({
+        "content-type": "application/x-www-form-urlencoded"
+    });
+
+    req.form({});
+
+    req.end((res: any) => {
+        response.status(res.status ? res.status : 500);
+        if (!res.body && res.error) {
+            response.redirect(query.state + toSearchString({
+                ...query,
+                error: "unexpected error",
+                error_description: "unexpected error"
+            }));
+        } else {
+            response.redirect(query.state + toSearchString({...query, ...res.body}));
+        }
+    });
+});
+
+app.get("/v1/auth/refresh_token", (request, response) => {
+    response.set('Access-Control-Allow-Origin', 'https://marmer.github.io');
+
+    const query: { state: string, refresh_token: string, scope: string, authuser: string, prompt: string, error?: string } = {...request.query};
+
+    const req = unirest("POST", "https://oauth2.googleapis.com/token");
+
+    if (query.error) {
+        response.redirect(query.state + toSearchString(query as any))
+    }
+    if (!query.refresh_token) {
+        response.redirect("Missing parameter refresh_token")
+    }
+
+    req.timeout(30000);
+    req.query({
+        "refresh_token": query.refresh_token,
+        "client_id": "502499757378-a54o9nthuijpls8ihifb37tltl76tvgj.apps.googleusercontent.com",
+        "grant_type": "refresh_token",
         "client_secret": clientSecret
     });
 
